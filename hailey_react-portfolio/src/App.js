f71e2b95c7f4f6c1c56769b0ba63fe464b917e32
Hailey Francis <n01402670@unf.edu>
2023-04-06 18:24:11 -0400

    started templating page format

---
+++ hailey_react-portfolio/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./components/Header";
+import Section from "./components/Section";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    //fragment
+  <>
+    <Header />
+    {/* <Content /> */}
+    {/* <Footer /> */}
+  </>
+  );
+}
+
+export default App;
---
+++ hailey_react-portfolio/src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <>
+      <header class="">
+            <h1 class="">Hailey D. Francis</h1>
+            <nav class="">
+                <ul class="">
+                    <li class=""><a class="" href="#About-Me">About Me</a></li>
+                    <li class=""><a class="" href="#Work">Work</a></li>
+                    <li class=""><a class="" href="#Contact-Me">Contact Me</a></li>
+                    <li class=""><a class="" href="#Resume">Resume</a></li>
+                </ul>
+            </nav>
+        </header>
+    </>
+  )
+}
