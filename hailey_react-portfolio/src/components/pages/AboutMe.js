bdad22ba9a51453ccd1635e823199a6c382ee0dd
Hailey Francis <n01402670@unf.edu>
2023-04-09 20:12:06 -0400

    added images and link overlays to portfolio projects

--- hailey_react-portfolio/src/components/pages/AboutMe.js
+++ hailey_react-portfolio/src/components/pages/AboutMe.js
@@ -1,13 +1,13 @@
 import React from 'react'
-
+import "../../styles/AboutMe.css"
 export default function AboutMe() {
   return (
-    //TODO: Figure out how to put image
     <>
-        <div>IMAGE GOES HERE</div>
-        <p>I graduated in Spring 2022 from the University of North Florida with a degree in Computer Science and a Minor in Mathematics. I am currently enrolled in UCF's October 2022 Part-Time 24 Week Web Development bootcamp in an attempt to gain better professional skillset to succeed in the workforce.</p> 
-        <p>I'm a huge fan of video games and have been for most of my life, especially franchises like Pokémon, Fire Emblem, and Story of Seasons. In my free time, I've started learning about ROM hacking Generation III Pokémon Games, which has only furthered my interest in programming.</p> 
-        <p>I'm hoping that my education through college, my computing-related hobbies, and and UCF's Bootcamp program will help me open doors to jobs in web development or software development.</p>
+      <h1>About Me</h1>
+      <img src="./images/profilePic.png" class="profileImg" alt="Hailey Francis Profile Pic"></img>
+      <p>I graduated in Spring 2022 from the University of North Florida with a degree in Computer Science and a Minor in Mathematics. I am currently enrolled in UCF's October 2022 Part-Time 24 Week Web Development bootcamp in an attempt to gain better professional skillset to succeed in the workforce. I am scheduled to graduate this program at the beginning of May 2023.</p> 
+      <p>I'm a huge fan of video games and have been for most of my life, especially franchises like Pokémon, Fire Emblem, and Story of Seasons. In my free time, I've started learning about ROM hacking Generation III Pokémon Games, which has only furthered my interest in programming.</p> 
+      <p>I'm hoping that my education through college, my computing-related hobbies, and and UCF's Bootcamp program will help me open doors to jobs in web development or software development.</p>
     </>
   )
 }
--- hailey_react-portfolio/src/components/pages/Portfolio.js
+++ hailey_react-portfolio/src/components/pages/Portfolio.js
@@ -1,9 +1,45 @@
 import React from 'react'
+import "../../styles/Portfolio.css"
 
 export default function Portfolio() {
   return (
-    <div>
-      
-    </div>
+    <>
+    <h1>Portfolio</h1>
+    <figure class="container">
+      <img src="./images/recibeet.jpg" alt="ReciBEET" class="image"></img>
+      <div class="middle">
+        <a class="text" href="https://recibeet-2.herokuapp.com/">ReciBEET</a>
+      </div>
+    </figure>
+
+    <figure class="container">
+      <img src="./images/BeatsEats.png" alt="Beats And Eats" class="image"></img>
+      <div class="middle">
+        <a class="text" href="https://shobannah.github.io/BeatsAndEats/">Beats & Eats</a>
+      </div>
+    </figure>
+
+    <figure class="container">
+      <img src="./images/TechBlog.jpg" alt="Tech Blog" class="image"></img>
+      <div class="middle">
+        <a class="text" href="https://haileyf-techblog-app.herokuapp.com/">Tech Blog</a>
+      </div>
+    </figure>
+
+    <figure class="container">
+      <img src="./images/WeatherDashboard.jpg" alt="Weather Dashboard" class="image"></img>
+      <div class="middle">
+        <a class="text" href="https://hfrancis7.github.io/Module6-WeatherDashboard/">Weather Dashboard</a>
+      </div>
+    </figure>
+
+    <figure class="container">
+      <img src="./images/CodingQuiz.jpg" alt="Coding Quiz" class="image"></img>
+      <div class="middle">
+        <a class="text" href="https://hfrancis7.github.io/Module4Challenge-CodingQuiz/">Coding Quiz</a>
+      </div>
+    </figure>
+
+    </>
   )
 }
